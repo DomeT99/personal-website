46a0d4e7ab081e89f3fe0e359f6864bd31c67f04
Domenico Tenace <domenicotenace99@gmail.com>
2026-05-26 00:23:01 +0200

    feat: add new article and rss handle

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async (context) => {
+  const posts = await getCollection('posts');
+  
+  return rss({
+    title: 'Domenico Tenace - Software Developer',
+    description: 'Software developer passionate about the IT world and everything related to it',
+    site: context.site ?? 'https://domenicotenace.dev',
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: new Date(post.data.date),
+      link: `/blog/${post.id}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+};
